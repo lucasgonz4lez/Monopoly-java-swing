eef38b39289139b060b74677458615f8052116d3
LucasGPan <gid66473@educastur.es>
2025-11-17 11:00:43 +0100

    Base del proyecto

---
+++ Monopoly/src/ui/MainWindow.java
@@ -0,0 +1,12 @@
+package ui;
+import javax.swing.*;
+
+public class MainWindow {
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Monopoly");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+}
---
+++ Monopoly/src/ui/custom/CasillaPanel.java
@@ -0,0 +1,5 @@
+package ui.custom;
+
+public class CasillaPanel {
+    
+}
